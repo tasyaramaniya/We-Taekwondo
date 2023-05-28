41eac8d87c3fccf4bf130f04f507fe04dc53c590
tasyaramaniya <93901540+tasyaramaniya@users.noreply.github.com>
2023-05-28 19:20:05 +0700

    main

---
+++ Website We Taekwondo/script.js
@@ -0,0 +1,22 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    spaceBetween:true,
+    grabCursor:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+  });
